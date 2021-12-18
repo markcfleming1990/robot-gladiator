9a4499a2366343f674f0620a4006c68861e209aa
Mark <markcfleming1990@gmail.com>
2021-12-18 07:35:40 -0600

    stopping for now

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
+var playerName=windows.prompt("What is your robot's name?");
+console.log(playerName);
+
+
 // this creates a function named "fight"
+
 
 function fight() {
   window.alert("The fight has begun!");
 }
+
+
+fight();
